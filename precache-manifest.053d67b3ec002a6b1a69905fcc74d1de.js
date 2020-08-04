320517d40aedf9b3d9fadb7cc8004852072ed81c
Tom-Checkley99 <57465435+Tom-Checkley99@users.noreply.github.com>
2020-08-04 19:56:16 +0100

    Add files via upload

---
+++ precache-manifest.053d67b3ec002a6b1a69905fcc74d1de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f05d59fabe10a06978569c17e638e41d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "541f8459eeb4d07fcf4b",
+    "url": "/static/css/main.c4126a7e.chunk.css"
+  },
+  {
+    "revision": "d53a96c8a4319ba138c4",
+    "url": "/static/js/2.1f8139fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1f8139fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "541f8459eeb4d07fcf4b",
+    "url": "/static/js/main.6e4b8139.chunk.js"
+  },
+  {
+    "revision": "683dc1968fac3a61a77f",
+    "url": "/static/js/runtime-main.37ba2010.js"
+  }
+]);
